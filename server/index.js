806a53d3a5b6429eacafdd1b5ca655a47a7fe389
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-03-30 01:24:11 +0200

    delete data

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-
-
 const app = express();
 
 // Middleware
@@ -18,14 +16,11 @@ const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI ;
 
 // MongoDB connect
-mongoose.set("strictQuery", true);
 
 mongoose.connect(uri).then(() => {console.log("Connected to MongoDB");})
   .catch((err) => {
     console.error("Error connecting to MongoDB", err);
   });
-
-
 
 // schema
 const userSchema = new mongoose.Schema({
@@ -48,14 +43,19 @@ app.get("/", async(req, res) => {
 app.post("/create", async(req, res) => {
 const data = new userModel(req.body);
   await data.save();
-  res.send({success:true, data:"data created",data : data} );
-  console.log(req.body);
+  res.send({success:true, data:"Data created",data : data} );
 });
 
 // update data
 app.put("/update", async (req, res) => {
   const data = await userModel.findByIdAndUpdate(req.body._id, req.body, { new: true });
   res.json({ success: true, message: "Data updated", data });
+});
+
+// delete data
+app.delete("/delete/:id", async (req, res) => {
+  const data = await userModel.findByIdAndDelete(req.params.id);
+  res.json({ success: true, message: "Data deleted", data });
 });
 
 // start the server
