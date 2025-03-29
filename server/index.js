42bbb488ba7498d1c2e1ee2385636f36c01a9b2c
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-03-29 23:49:08 +0200

    add MongoDB connection and dotenv

--- server/index.js
+++ server/index.js
@@ -1,16 +1,35 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+
+
 const app = express();
+
+// Middleware
 app.use(cors());
 
+// port
 const PORT = process.env.PORT || 5000;
 
+// MongoDB connection
+const uri = process.env.MONGODB_URI ;
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect(uri).then(() => {console.log("Connected to MongoDB");})
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
+
+
+// root route
 app.get("/", (req, res) => {
   res.send("ososa");
 });
 
+// start the server
 app.listen(PORT, () => {
   console.log(` port ${PORT} is running`);
 });
